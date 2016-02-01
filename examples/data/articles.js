4d423745afb7362096bda1a7e359638c970e3ae6
Joshua Comeau <joshwcomeau@gmail.com>
2016-02-01 07:36:03 -0500

    Refactor and rearrange Shuffle
    Got Articles put into a 'data' directory, refactored the method
    order to be as straightforward as possible, few small tweaks.
    
    Moved 1_Shuffle into /components.

---
+++ examples/data/articles.js
@@ -0,0 +1,11 @@
+const articles = [
+  { id: 'a', timestamp: 1401336000000, name: 'The Dawn of Time' },
+  { id: 'b', timestamp: 1426305600000, name: 'A While Back' },
+  { id: 'c', timestamp: 1439006400000, name: 'This Just Happened' },
+  { id: 'd', timestamp: 1451710800000, name: 'Another Headline' },
+  { id: 'e', timestamp: 1452315600000, name: 'Whatever will we do' },
+  { id: 'f', timestamp: 1453525200000, name: 'Words' },
+  { id: 'g', timestamp: 1500000000000, name: 'This Just Happened' }
+]
+
+export default articles;
