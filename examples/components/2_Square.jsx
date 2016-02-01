640a50456f3eaf6ada6ee44b5b9fcb72a2ddd8ea
Joshua Comeau <joshwcomeau@gmail.com>
2016-02-01 08:34:33 -0500

    Start implementing functionality for Square
    Imported a nifty library to help with keydown events (react-keydown).
    Monkeypatched Array to have a 'move' method, so that I'll easily be
    able to change a DOM element's position based on the events.
    
    All the pieces are there, they just need to be wired up =)

--- examples/components/2_Square.jsx
+++ examples/components/2_Square.jsx
@@ -2,13 +2,23 @@ import React, { Component, PropTypes }  from 'react';
 import moment                           from 'moment';
 import { times }                        from 'lodash';
 import classNames                       from 'classnames';
+import keydown, { Keys }                from 'react-keydown';
 
 import FlipMove from '../TEMP_flip-move';
 
 
 const NUM_SQUARES = Math.pow(5, 2);
-const RED_SQUARE  = Math.floor(NUM_SQUARES / 2)
+const RED_SQUARE  = Math.floor(NUM_SQUARES / 2);
+const { UP, DOWN, LEFT, RIGHT } = Keys;
 
+// Monkeypatching is bad, but so much fun (=
+Array.prototype.move = function (old_index, new_index) {
+    if (new_index >= this.length) return;
+    this.splice(new_index, 0, this.splice(old_index, 1)[0]);
+    return this; // for testing purposes
+};
+
+@keydown(UP, DOWN, LEFT, RIGHT)
 class Board extends Component {
   constructor(props) {
     super(props);
@@ -24,6 +34,18 @@ class Board extends Component {
     return this.state.squares.map( square => (
       <div className="square" id={ square.red ? 'red' : null } />
     ));
+  }
+
+  componentWillReceiveProps( { keydown } ) {
+    if ( keydown.event ) {
+      // inspect the keydown event and decide what to do
+      console.log( keydown.event.which );
+    }
+  }
+
+  move(event) {
+    // TODO: Directions aside from right =)
+    console.log("Move fired!", this, event)
   }
 
   render() {
