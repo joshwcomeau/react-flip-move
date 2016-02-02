bf8ea9e2cb4d77aea0987bb5e878370cdd0a6b39
Josh Comeau <josh@breather.com>
2016-02-01 19:46:40 -0500

    Set keydown to work on global component
    Instead of needing the child component to be in focus, set up
    keydown to work on any component; made clever use of the keydownScoped
    method.

--- examples/index.jsx
+++ examples/index.jsx
@@ -2,6 +2,7 @@ import React, { Component, PropTypes }  from 'react';
 import ReactDOM, { render }             from 'react-dom';
 import classNames                       from 'classnames';
 import { Router, Route, Link }          from 'react-router';
+import keydown, { Keys }                from 'react-keydown';
 
 import Header   from './Components/Header.jsx';
 import Shuffle  from './Components/1_Shuffle.jsx';
@@ -10,12 +11,16 @@ import Square   from './Components/2_Square.jsx';
 require('./scss/main.scss');
 
 
+// Capture keydown events app-wide.
+// Descendant components may use these keys to perform actions
+// eg. the Square demo uses them to move a red square about =)
+@keydown
 class App extends Component {
   render() {
     return (
       <div className="app">
         <Header />
-        { this.props.children }
+        { React.cloneElement(this.props.children, {keydown: this.props.keydown}) }
       </div>
     );
   }
