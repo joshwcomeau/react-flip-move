0227e753877b18610cab1c296992e8c8eda48dc4
Joshua Comeau <joshwcomeau@gmail.com>
2016-02-03 07:45:10 -0500

    Separate and improve Footer data

---
+++ examples/data/details.js
@@ -0,0 +1,14 @@
+const data = {
+  shuffle: {
+    title: 'List/Grid Reordering',
+    details: 'Handle smooth re-ordering of arbitrarily-sized DOM nodes.\nUses staggered duration to make the animation feel more organic.',
+    source: 'https://github.com/joshwcomeau/react-flip-move/blob/gh-pages/examples/components/1_Shuffle.jsx'
+  },
+  square: {
+    title: 'Fuscia Square',
+    details: 'Use your arrow keys to move a square about.\nA silly experiment that makes use of a cubic-bezier easing and start/finish callbacks.',
+    source: 'https://github.com/joshwcomeau/react-flip-move/blob/gh-pages/examples/components/2_Square.jsx'
+  }
+};
+
+export default data;
