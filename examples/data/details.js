8dd0c2e5b1b614fef247ad6e055a0e3373317714
Joshua Comeau <joshwcomeau@gmail.com>
2016-02-05 08:49:11 -0500

    Add details for Scrabble demo

--- examples/data/details.js
+++ examples/data/details.js
@@ -8,7 +8,13 @@ const data = {
     title: 'Fuscia Square',
     details: 'Use your arrow keys to move a square about.\nA silly experiment that makes use of a cubic-bezier easing and start/finish callbacks.',
     source: 'https://github.com/joshwcomeau/react-flip-move/blob/gh-pages/examples/components/2_Square.jsx'
-  }
+  },
+  scrabble: {
+    title: 'Scrabble',
+    details: 'Flip move plays nice with React DnD.\nTry dropping tiles on each other to swap, or hit "reset" to revert to the original board.',
+    source: 'https://github.com/joshwcomeau/react-flip-move/blob/gh-pages/examples/components/3_Scrabble.jsx'
+  },
+
 };
 
 export default data;
