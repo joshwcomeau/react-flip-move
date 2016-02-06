56df900f64e93166822e1345334bce15671f0c06
Joshua Comeau <joshwcomeau@gmail.com>
2016-02-06 16:15:20 -0500

    Prepare examples for deploy
    Updated webpack to work in both dev and prod. Fixed a few small
    details.

--- examples/data/articles.js
+++ examples/data/articles.js
@@ -2,8 +2,8 @@ const articles = [
   { id: 'a', timestamp: 811396800000, name: 'Netscape 2.0 ships, introducing Javascript' },
   { id: 'b', timestamp: 1108702800000, name: 'Jesse James Garrett releases AJAX spec' },
   { id: 'c', timestamp: 1156564800000, name: 'jQuery 1.0 released' },
-  { id: 'e', timestamp: 1256443200000, name: 'First underscore.js commit' },
-  { id: 'd', timestamp: 1286942400000, name: 'Backbone.js becomes a thing' },
+  { id: 'd', timestamp: 1256443200000, name: 'First underscore.js commit' },
+  { id: 'e', timestamp: 1286942400000, name: 'Backbone.js becomes a thing' },
 
   { id: 'f', timestamp: 1331697600000, name: 'Angular 1.0 released' },
   { id: 'g', timestamp: 1369800000000, name: 'React is open-sourced; developers rejoice' }
--- examples/webpack.config.development.js
+++ examples/webpack.config.development.js
@@ -1,6 +1,5 @@
 var path = require('path');
 var webpack = require('webpack');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
@@ -18,7 +17,6 @@ module.exports = {
   },
 
   plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ],
