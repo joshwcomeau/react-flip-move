764763e0a3d57ea4b46dc4cad820bc344430c9b7
Joshua Comeau <joshwcomeau@gmail.com>
2016-02-06 09:40:18 -0500

    Throttle and refine Square demo
    The Fuscia Square demo was buggy and awkward. To improve things, I
    wrote a quick throttle function that ignores multiple requests, that
    way the square's movement is predictable and never moves more than 1
    place in the timeframe specified.
    
    For organization, I moved this function into a new `helpers` dir,
    along with the monkeypatched Array methods `range` and `matrix`.

---
+++ examples/helpers/throttle.js
@@ -0,0 +1,24 @@
+/**
+ * Throttle.
+ * A very simple throttle function. Differs from traditional throttle
+ * implementations in that it does NOT batch events.
+ * For example, if it's set for 250ms, and it receives 5 events in that
+ * window, it only fires once.
+ * @param {Function} fn - the function to throttle
+ * @param {Object} options:
+ *   @param {Number} threshold - The maximum fire rate of the fn
+ *   @param {Object} context - The `this` value that should be passed to fn
+ * @returns {Function}
+ */
+function throttle(fn, { threshold = 200, context = window }) {
+  let last;
+
+  return function () {
+    const now = +new Date;
+
+    if ( !last || now >= (last + threshold) ) {
+      last = now;
+      fn.apply(context, arguments);
+    }
+  };
+}
