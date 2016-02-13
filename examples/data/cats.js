572b9b1c4a425b6d5f05edf509cd626f4472c0a8
Joshua Comeau <joshwcomeau@gmail.com>
2016-02-13 09:13:00 -0500

    Populate cat data

--- examples/data/cats.js
+++ examples/data/cats.js
@@ -1,12 +1,47 @@
 const cats = [
-  { id: 'a', timestamp: 811396800000, name: 'Netscape 2.0 ships, introducing Javascript' },
-  { id: 'b', timestamp: 1108702800000, name: 'Jesse James Garrett releases AJAX spec' },
-  { id: 'c', timestamp: 1156564800000, name: 'jQuery 1.0 released' },
-  { id: 'd', timestamp: 1256443200000, name: 'First underscore.js commit' },
-  { id: 'e', timestamp: 1286942400000, name: 'Backbone.js becomes a thing' },
-
-  { id: 'f', timestamp: 1331697600000, name: 'Angular 1.0 released' },
-  { id: 'g', timestamp: 1369800000000, name: 'React is open-sourced; developers rejoice' }
+  {
+    id: 'a',
+    name: 'Maru',
+    origin: 'Japan',
+    breed: 'Scottish Fold',
+    catchphrase: 'Master of Boxes',
+    img: 'https://s3.amazonaws.com/githubdocs/cats_1_maru.jpg',
+    url: 'http://sisinmaru.blog17.fc2.com/'
+  },
+  {
+    id: 'b',
+    name: 'Venus',
+    origin: 'United States',
+    breed: 'Unknown',
+    catchphrase: 'The Chimera Cat',
+    img: 'https://s3.amazonaws.com/githubdocs/cats_2_venus.jpg',
+    url: 'https://www.facebook.com/VenusTheAmazingChimeraCat'
+  },
+  {
+    id: 'c',
+    name: 'Honey Bee',
+    origin: 'Fiji',
+    breed: 'Unknown',
+    catchphrase: 'Blind Hiking Cat',
+    img: 'https://s3.amazonaws.com/githubdocs/cats_3_honeybee.jpg',
+    url: 'https://www.facebook.com/blindcathoneybee'
+  },
+  {
+    id: 'd',
+    name: 'Banye',
+    origin: 'China',
+    breed: 'British Shorthair',
+    img: 'https://s3.amazonaws.com/githubdocs/cats_4_banye.jpg',
+    catchphrase: 'Perpetually Surprised'
+  },
+  {
+    id: 'e',
+    name: 'Snoopy',
+    origin: 'China',
+    breed: 'Exotic Shorthair',
+    img: 'https://s3.amazonaws.com/githubdocs/cats_5_snoopy.jpg',
+    catchphrase: 'Not to be Confused with the Dog'
+  },
 ]
 
 export default cats;
