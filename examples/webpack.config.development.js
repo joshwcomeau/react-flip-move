71a2a86552d000f63f29396a939dbd158f844a2a
Joshua Comeau <joshwcomeau@gmail.com>
2016-06-03 08:19:57 -0400

    Use react-flip-move v2.4.0-rc2

--- examples/webpack.config.development.js
+++ examples/webpack.config.development.js
@@ -2,8 +2,6 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
-
   entry: [
     'webpack-hot-middleware/client',
     './index.js'
