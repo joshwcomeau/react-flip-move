2a241443a664edc6000431218bc8bf9828d65345
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2017-05-14 14:59:38 -0500

    Remove css and style loaders

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,16 +2,6 @@
 const path = require('path');
 
 module.exports = {
-  module: {
-    loaders: [
-      {
-        test: /css$/,
-        loaders: [ 'style', 'css' ],
-        include: path.resolve(__dirname, '../')
-      }
-    ]
-  },
-
   resolve: {
     alias: (process.env.REACT_IMPL === "preact") ? {
       "react": "preact-compat",
